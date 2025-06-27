f580e373b1f512b6e552ebefee17904233cb89b7
Thisharu-Dilhara <thisharud@gmail.com>
2025-06-27 20:42:59 +0530

    add DateTimeFormatter and LocalTime

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmClock;
+        try{
+            System.out.print("Enter an alarm time (HH:MM:SS): ");
+            String inputTime = scanner.nextLine();
+
+            alarmClock = LocalTime.parse(inputTime, formatter);
+            System.out.println("Alarm set for " + alarmClock);
+
+        }
+        catch (DateTimeParseException e){
+            System.out.println("Enter (HH:MM:SS) this order ");
+        }
+
+        scanner.close();
 
 
     }
